ddf2311d8e66f871d9c0c1afcce8db1076bef59e
Sebastian Schmidt <schmidt.sebastian2@fh-swf.de>
2022-07-27 09:41:13 +0200

    Updated: Calculation of Elevation Textures

--- Assets/Scripts/Views/GameViews/MapTileViews.cs
+++ Assets/Scripts/Views/GameViews/MapTileViews.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using Model.Map;
 using UnityEngine;
@@ -29,12 +30,13 @@ namespace Views.GameViews
             }
         }
 
+        private List<float> _heightSteps = new();
+        [SerializeField]
+        private float lowestHeight = -8000;
         [SerializeField] 
-        private float[] heightSteps = 
-        {
-            -8000, -6000, -4000, -2000, -1000, -500, -200, -100, -50,
-            0, 50, 100, 200, 500, 1000, 2000, 4000, 6000, 8000, 10000
-        };
+        private float heighestHeight = 10000;
+        [SerializeField] 
+        private float heightStep = 1000;
 
         [SerializeField]
         private float minHeightColor = 0.3f;
@@ -51,12 +53,15 @@ namespace Views.GameViews
 
         protected void OnEnable()
         {
-            TestTile = new Tile[heightSteps.Length];
-            TestTile2 = new Tile[heightSteps.Length];
+            CalculateHeightSteps();
+            TestTile = new Tile[_heightSteps.Count];
+            TestTile2 = new Tile[_heightSteps.Count];
 
-            _colorStep = (1.0f - minHeightColor) / heightSteps.Length;
+            Debug.Log("Height Steps: " + TestTile.Length);
+
+            _colorStep = (1.0f - minHeightColor) / _heightSteps.Count;
             
-            for (int i = 0; i < heightSteps.Length; i++)
+            for (int i = 0; i < _heightSteps.Count; i++)
             {
                 float colorValue = 1.0f - _colorStep * i;
                 Color newColor = new Color(colorValue, colorValue, colorValue, 1.0f);
@@ -72,11 +77,23 @@ namespace Views.GameViews
             }
         }
 
+        private void CalculateHeightSteps()
+        {
+            _heightSteps = new List<float>();
+            float currentHeight = lowestHeight;
+            do
+            {
+                _heightSteps.Add(currentHeight);
+                currentHeight += heightStep;
+            } while (currentHeight < heighestHeight);
+            _heightSteps.Add(currentHeight);
+        }
+
         public Color GetTileColorOffsetByElevation(float elevation)
         {
-            for (int i = 0; i < heightSteps.Length; i++)
+            for (int i = 0; i < _heightSteps.Count; i++)
             {
-                if (heightSteps[i] > elevation)
+                if (_heightSteps[i] > elevation)
                 {
                     float colorValue = 1.0f - _colorStep * Math.Max(i - 1, 0);
                     return new Color(colorValue, colorValue, colorValue, 1.0f);
@@ -88,9 +105,9 @@ namespace Views.GameViews
         
         public Tile GetTileByMapUnit([NotNull] MapUnit mapUnit)
         {
-            for (int i = 0; i < heightSteps.GetLength(0); i++)
+            for (int i = 0; i < _heightSteps.Count; i++)
             {
-                if (heightSteps[i] > mapUnit.Position.Elevation)
+                if (_heightSteps[i] > mapUnit.Position.Elevation)
                 {
                     int take = Math.Max(i - 1, 0);
                     return TestTile[take];
