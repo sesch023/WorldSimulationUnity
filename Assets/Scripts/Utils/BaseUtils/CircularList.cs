5aac51a0af7314a78bd2598d8d3d04d8b7317c90
Sebastian Schmidt <schmidt.sebastian2@fh-swf.de>
2022-11-16 07:47:56 +0100

    Changed: Public Circular List

---
+++ Assets/Scripts/Utils/BaseUtils/CircularList.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Utils.BaseUtils
+{
+    public class CircularList<T> : List<T>
+    {
+        public T Last
+        {
+            get { return this[this.Count - 1]; }
+            set { this[this.Count - 1] = value; }
+        }
+
+        public T First
+        {
+            get { return this[0]; }
+            set { this[0] = value; }
+        }
+
+        public void PushLast(T obj)
+        {
+            this.Add(obj);
+        }
+
+        public T PopLast()
+        {
+            T retVal = this[this.Count - 1];
+            this.RemoveAt(this.Count - 1);
+            return retVal;
+        }
+
+        public void PushFirst(T obj)
+        {
+            this.Insert(0, obj);
+        }
+
+        public T PopFirst()
+        {
+            T retVal = this[0];
+            this.RemoveAt(0);
+            return retVal;
+        }
+    }
+}
--- Assets/Scripts/Utils/BaseUtils/ConvexHull.cs
+++ Assets/Scripts/Utils/BaseUtils/ConvexHull.cs
@@ -65,56 +65,5 @@ namespace Utils.BaseUtils
         {
             return a.x * b.y - a.y * b.x;
         }
-
-        private class CircularList<T> : List<T>
-        {
-            public T Last
-            {
-                get
-                {
-                    return this[this.Count - 1];
-                }
-                set
-                {
-                    this[this.Count - 1] = value;
-                }
-            }
-
-            public T First
-            {
-                get
-                {
-                    return this[0];
-                }
-                set
-                {
-                    this[0] = value;
-                }
-            }
-
-            public void PushLast(T obj)
-            {
-                this.Add(obj);
-            }
-
-            public T PopLast()
-            {
-                T retVal = this[this.Count - 1];
-                this.RemoveAt(this.Count - 1);
-                return retVal;
-            }
-
-            public void PushFirst(T obj)
-            {
-                this.Insert(0, obj);
-            }
-
-            public T PopFirst()
-            {
-                T retVal = this[0];
-                this.RemoveAt(0);
-                return retVal;
-            }
-        }
     }
 }
