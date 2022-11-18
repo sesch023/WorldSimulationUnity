4714ab8a11db8bf7af066eb8f844036d68f40bcf
Sebastian Schmidt <schmidt.sebastian2@fh-swf.de>
2022-11-18 10:03:01 +0100

    Added: Normalized Float Array

---
+++ Assets/Scripts/Utils/Arrays/NormalizedFloatArray.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Utils.Arrays
+{
+    public class NormalizedFloatArray : IArray<float>, IFixedDimensional
+    {
+        public float[] Data { get; }
+        private float _minimum;
+        private float _maximum;
+        private float _range;
+
+        public NormalizedFloatArray(float[] data)
+        {
+            Data = data;
+            _minimum = data.Min();
+            _maximum = data.Max();
+            _range = _maximum - _minimum;
+        }
+
+        public IEnumerator<float> GetEnumerator()
+        {
+            return (IEnumerator<float>) Data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int GetLength(int dimension)
+        {
+            return Data.GetLength(0);
+        }
+
+        public float this[int x]
+        {
+            get => (Data[x] - _minimum) / _range;
+            set
+            {
+                value = value * _range + _minimum;
+                if (value > _maximum)
+                {
+                    _maximum = value;
+                    _range = _maximum - _minimum;
+                }
+                else if (value < _minimum)
+                {
+                    _minimum = value;
+                    _range = _maximum - _minimum;
+                }
+                
+                Data[x] = value;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Arrays/NormalizedFloatArray2D.cs
@@ -0,0 +1,49 @@
+﻿namespace Utils.Arrays
+{
+    public class NormalizedFloatArray2D : I2DArray<float>, IFixedDimensional
+    {
+        public float[,] Data { get; }
+        private float _minimum;
+        private float _maximum;
+        private float _range;
+        
+        public NormalizedFloatArray2D(float[,] data)
+        {
+            Data = data;
+            _minimum = BaseUtils.Util.MinIn2DArray(data);
+            _maximum = BaseUtils.Util.MaxIn2DArray(data);
+            _range = _maximum - _minimum;
+        }
+        
+        public I2DEnumerator<float> Get2DEnumerator()
+        {
+            return new Array2DEnumerator<float>(new ArrayImmutable2D<float>(Data));
+        }
+
+        public int GetLength(int dimension)
+        {
+            return Data.GetLength(dimension);
+        }
+
+        public float this[int x, int y]
+        {
+            get => (Data[x, y] - _minimum) / _range;
+            set
+            {
+                value = value * _range + _minimum;
+                if (value > _maximum)
+                {
+                    _maximum = value;
+                    _range = _maximum - _minimum;
+                }
+                else if (value < _minimum)
+                {
+                    _minimum = value;
+                    _range = _maximum - _minimum;
+                }
+                
+                Data[x, y] = value;
+            }
+        }
+    }
+}
