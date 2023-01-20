7a85dcc9e9fc950bf5d458dc0eaa4675b3044718
Sebastian Schmidt <schmidt.sebastian2@fh-swf.de>
2023-01-20 11:24:55 +0100

    Added: Sun

---
+++ Assets/Scripts/Model/Map/Feature/Sun.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Model.Map.Feature
+{
+    public class Sun : IFeature
+    {
+        
+        
+        public Vector2Int[] GetFeaturePositions()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
